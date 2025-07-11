7c3696985d5fabfe5d726179fdb5864c6f27d34c
Khoai Tay Chien <trangbth22411@st.uel.edu.vn>
2025-07-11 22:37:05 +0700

    Resolve merge conflict giữa login-logout và main

--- app/src/main/java/com/group4/ui/user/Register_Activity.java
+++ app/src/main/java/com/group4/ui/user/Register_Activity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import android.os.Bundle;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
